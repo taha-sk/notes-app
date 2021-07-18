4a96a70342d67459a1a8fa9b54dc3dadea3bee79
Taha Sukru Karabacakoglu <supersawer@gmail.com>
2021-07-18 15:56:09 +0300

    Fixing tests

--- __tests__/EditNoteScreen-test.js
+++ __tests__/EditNoteScreen-test.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import renderer from 'react-test-renderer';
-import { render, fireEvent } from '@testing-library/react-native';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
 
 import EditNoteScreen from '../screens/EditNoteScreen';
 
@@ -24,7 +24,7 @@ describe('<EditNoteScreen />', () => {
     expect(tree.children.length).toBe(2);
   });
 
-  it('saves correctly', () => {
+  it('saves correctly', async () => {
     const EditNoteScreenProps = {
       navigation: {goBack: jest.fn()},
       route: {params: { noteId: "1"}}
@@ -34,20 +34,20 @@ describe('<EditNoteScreen />', () => {
       getByPlaceholderText('Type here to add your note!'),
       'test note'
     );
-     const saveBtn = getByTestId('save-btn');
-     fireEvent.press(saveBtn);
-    expect(EditNoteScreenProps.navigation.goBack).toHaveBeenCalledTimes(1);
+    const saveBtn = getByTestId('save-btn');
+    fireEvent.press(saveBtn);
+    await waitFor(() => expect(EditNoteScreenProps.navigation.goBack).toHaveBeenCalled());
   });
 
-  it('deletes correctly', () => {
+  it('deletes correctly', async () => {
     const EditNoteScreenProps = {
       navigation: {goBack: jest.fn()},
       route: {params: { noteId: "1"}}
     };
     const { getByTestId } = render(<EditNoteScreen {...EditNoteScreenProps} />);
-     const deleteBtn = getByTestId('delete-btn');
-     fireEvent.press(deleteBtn);
-    expect(EditNoteScreenProps.navigation.goBack).toHaveBeenCalledTimes(1);
+    const deleteBtn = getByTestId('delete-btn');
+    fireEvent.press(deleteBtn);
+    await waitFor(() => expect(EditNoteScreenProps.navigation.goBack).toHaveBeenCalled());
   });  
 
 });
